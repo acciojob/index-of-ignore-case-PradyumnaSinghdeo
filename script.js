078b4509fe1ed6ac28ef2414a68923338bd09cb9
Pradyumna Singh Deo <87163520+PradyumnaSinghdeo@users.noreply.github.com>
2024-08-21 22:19:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	 // Convert both strings to lowercase for case-insensitive comparison
+    const lowerStr = str.toLowerCase();
+    const lowerSubStr = subStr.toLowerCase();
+
+    // Use indexOf to find the first occurrence of lowerSubStr in lowerStr
+    return lowerStr.indexOf(lowerSubStr);
+}
+
+// Please do not change the code below
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(indexOfIgnoreCase(s1, s2));
 }
 
 // Please do not change the code below
